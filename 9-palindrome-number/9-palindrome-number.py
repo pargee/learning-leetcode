c675a1de9ffdc32d47d78a3ab32979bc791c2a90
Justin Kovac <justin@pargee.com>
2022-04-16 15:05:46 -0700

    Time: 67 ms (80.34%), Space: 14 MB (18.07%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,5 +1,6 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        # Convert the numbers to a string then reverse it (should be faster than a list?)
         num_string = str(x)
         if num_string == num_string[::-1]:
             return True
