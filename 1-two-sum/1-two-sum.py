481d37243d9b7a428dba1c5477e0a136da6724c0
Justin Kovac <justin@pargee.com>
2022-04-16 14:47:10 -0700

    Time: 541 ms (36.49%), Space: 14.9 MB (77.33%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,10 +1,17 @@
 class Solution:
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     map = {}
+    #     for n in range(len(nums)):
+    #         difference = target - nums[n]
+    #         if difference in map:
+    #             return [map[difference], n]
+    #         map[nums[n]] = n
+    #     return None
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        map = {}
-        
+        potentials = []
         for n in range(len(nums)):
             difference = target - nums[n]
-            if difference in map:
-                return [map[difference], n]
-            map[nums[n]] = n
+            if difference in potentials:
+                return [potentials.index(difference), n]
+            potentials.append(nums[n])
         return None
