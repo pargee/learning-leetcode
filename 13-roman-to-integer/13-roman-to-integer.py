29631521e251986b2282bde2d5f81a02a8b0db08
Justin Kovac <justin@pargee.com>
2022-04-16 18:36:58 -0700

    Time: 59 ms (66.29%), Space: 13.9 MB (33.30%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,33 +1,14 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        roman = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000,'IV':4,'IX':9,'XL':40,'XC':90,'CD':400,'CM':900}
+        i = 0
         num = 0
-        values = {
-            "I": 1,
-            "V": 5,
-            "X": 10,
-            "L": 50,
-            "C": 100,
-            "D": 500,
-            "M": 1000,
-
-        }
-        sub_values = {
-            "IV": 4,
-            "IX": 9,
-            "XL": 40,
-            "XC": 90,
-            "CD": 400,
-            "CM": 900
-        }
-
-        # Check for sub values first
-        for val in sub_values.keys():
-            if val in s:
-                num = num + sub_values[val]
-                s = s.replace(val, '')
-
-        # Make a list from the remaining chars
-        for val in s:
-            num = num + values[val]
-
+        while i < len(s):
+            if i+1<len(s) and s[i:i+2] in roman:
+                num+=roman[s[i:i+2]]
+                i+=2
+            else:
+                #print(i)
+                num+=roman[s[i]]
+                i+=1
         return num
