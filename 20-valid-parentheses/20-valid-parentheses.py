16090681a0c8bf2a604cd6ac6d8ea5b06e4de63d
Justin Kovac <justin@pargee.com>
2022-04-18 18:08:33 -0700

    Time: 36 ms (74.15%), Space: 13.9 MB (73.56%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,13 +1,19 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        accepted_pairs = ["()", "[]", "{}"]
-        
-        # Really not happy with this solution but need to step away from thinking about it
-        while True:
-            for pair in accepted_pairs:
-                if pair in s:
-                    s = s.replace(pair, '')
-            if len(s) == 0:
-                return True
-            if not any(pair in s for pair in accepted_pairs):
-                return False
+        st = list()
+
+        # Much cleaner example after doing some research
+        for c in s:
+            if c == "(" or c == "[" or c == "{":
+                st.append(c)
+            else:
+                if len(st) == 0:
+                    return False
+                if c == ")" and st[-1] != "(":
+                    return False
+                if c == "]" and st[-1] != "[":
+                    return False
+                if c == "}" and st[-1] != "{":
+                    return False
+                st.pop()
+        return len(st) == 0
