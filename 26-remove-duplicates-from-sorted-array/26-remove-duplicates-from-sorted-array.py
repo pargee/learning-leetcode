e592ef04e9fe02ac88c220ec059140217b38523a
Justin Kovac <justin@pargee.com>
2022-04-21 20:24:56 -0700

    Time: 134 ms (46.79%), Space: 15.7 MB (17.78%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -3,12 +3,32 @@ class Solution:
         # Had some trouble figuring out what the actual output was
         # Should try figuring out a different way to sort this
         x = 1
+        temp_l = []
         for index in range(len(nums) - 1):
             if nums[index] == nums[index + 1]:
                 nums[index] = None
             else:
                 x = x + 1
-        nums.sort(key=lambda x: (x is None, x))
+                temp_l.append(nums[index])
+        temp_l.append(nums[-1])
+
+        for index in range(len(nums)):
+            if index < len(temp_l):
+                nums[index] = temp_l[index]
+            else:
+                nums[index] = None
+                
         return x
+        
+        # Surprisingly, this is slower and more memory intensive (140MS / 16.8MB)
+        # Way cleaner though...
+        # x = 1
+        # for index in range(len(nums) - 1):
+        #     if nums[index] == nums[index + 1]:
+        #         nums[index] = None
+        #     else:
+        #         x = x + 1
+        # nums.sort(key=lambda x: (x is None, x))
+        # return x
                 
                 
