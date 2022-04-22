6a45e6c957f3f6524c274955751ceb30741fe760
Justin Kovac <justin@pargee.com>
2022-04-21 20:49:25 -0700

    Time: 142 ms (41.76%), Space: 15.5 MB (63.27%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -6,6 +6,8 @@ class Solution:
             if nums[index] == nums[index - 1]:
                 duplicates = duplicates + 1
             else:
+                # We don't care what the tail values are, so leave and just overwrite the previous duplicate
+                # Example, [1, 2, 2, 4] would move 4 to [2] (index = 3, duplicates = 1 or 3 - 1 = [2])
                 nums[index - duplicates] = nums[index]
         return len(nums) - duplicates
                 
